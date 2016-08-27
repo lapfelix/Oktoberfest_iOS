f11adf73d22df44a176b7d8aa661241b69f3a970
lapfelix <felix@lapal.me>
2016-08-27 13:08:49 -0400

    Add empty required methods to Core Data classes

--- BusPath+Methods.m
+++ BusPath+Methods.m
@@ -12,4 +12,25 @@ static NSString *entityName = @"BusPath";
 
 @implementation BusPath (Methods)
 
++ (NSString *)entityName {
+    return entityName;
+}
+
++ (instancetype) modelObjectWithDictionary:(NSDictionary *)dict managedObjectContext:(NSManagedObjectContext *)context {
+    return [[self alloc] initWithDictionary:dict managedObjectContext:context];
+}
+
+- (instancetype) initWithDictionary:(NSDictionary *)dict managedObjectContext:(NSManagedObjectContext *)context {
+    self = [NSEntityDescription insertNewObjectForEntityForName:entityName inManagedObjectContext: context];
+    if([dict isKindOfClass:[NSDictionary class]]) {
+        /*
+         self.id = [dict[@"id"] intValue];
+         self.alcohol = [dict[@"alcohol"] floatValue];
+         self.beerDescription = [dict objectOrNilForKey:@"description"];
+         self.name = [dict objectOrNilForKey:@"name"];
+         */
+    }
+    return self;
+}
+
 @end
