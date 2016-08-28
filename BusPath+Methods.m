bc4785f8482b5f58f869c2cb6fbdaee83f7b0198
lapfelix <felix@lapal.me>
2016-08-28 19:30:05 -0400

    Fix BusPath parsing

--- BusPath+Methods.m
+++ BusPath+Methods.m
@@ -26,7 +26,7 @@ static NSString *entityName = @"BusPath";
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         formatter = [[NSDateFormatter alloc] init];
-        formatter.dateFormat = @"MM-dd-yyyy HH:mm";
+        formatter.dateFormat = @"HH:mm";
     });
     return formatter;
 }
@@ -34,7 +34,7 @@ static NSString *entityName = @"BusPath";
 - (instancetype) initWithDictionary:(NSDictionary *)dict managedObjectContext:(NSManagedObjectContext *)context {
     self = [NSEntityDescription insertNewObjectForEntityForName:entityName inManagedObjectContext: context];
     if([dict isKindOfClass:[NSDictionary class]]) {
-        
+        self.id = [dict[@"id"] intValue];
         self.title = [dict objectOrNilForKey:@"title"];
         self.additionalString = [dict objectOrNilForKey:@"additionnal_string"];
         self.startTime = [self.class.dateFormatter dateFromString:[dict objectOrNilForKey:@"start_time"]];
