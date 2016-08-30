8e751cad605c6068110728aa711ab453dcb20744
lapfelix <felix@lapal.me>
2016-08-30 15:13:06 -0400

    Add images to schedule items (WIP)

--- ScheduleItem+Methods.m
+++ ScheduleItem+Methods.m
@@ -7,6 +7,7 @@
 //
 
 #import "ScheduleItem+Methods.h"
+#import <SDWebImage/SDWebImageManager.h>
 
 static NSString *entityName = @"ScheduleItem";
 
@@ -38,15 +39,49 @@ static NSString *entityName = @"ScheduleItem";
         self.startTime = [self.class.dateFormatter dateFromString:[dict objectOrNilForKey:@"start_time"]];
         self.endTime = [self.class.dateFormatter dateFromString:[dict objectOrNilForKey:@"end_time"]];
         self.name = [dict objectOrNilForKey:@"name"];
-        self.scheduleItemDescription = [dict objectForKey:@"description"];
+        self.scheduleItemDescription = [dict objectOrNilForKey:@"description"];
         
-        /*
-        self.alcohol = [dict[@"alcohol"] floatValue];
-        self.beerDescription = [dict objectOrNilForKey:@"description"];
-        self.name = [dict objectOrNilForKey:@"name"];
-        */
+        //TODO: make this nice
+        SDWebImageManager *manager = [SDWebImageManager sharedManager];
+        
+        NSString *largeImageURLString = [dict objectOrNilForKey:@"large_image_url"];
+        self.largeImage = largeImageURLString;
+        if (largeImageURLString != nil) {
+            NSURL *imageURL = [NSURL URLWithString:largeImageURLString];
+            
+            [manager downloadImageWithURL:imageURL
+                                  options:0
+                                 progress:nil
+                                completed:nil];
+        }
+        NSString *smallImageURLString = [dict objectOrNilForKey:@"large_image_url"];
+        self.smallImage = largeImageURLString;
+        if (smallImageURLString != nil) {
+            NSURL *imageURL = [NSURL URLWithString:smallImageURLString];
+            
+            [manager downloadImageWithURL:imageURL
+                                  options:0
+                                 progress:nil
+                                completed:nil];
+        }
+        
     }
     return self;
 }
 
+- (void)saveContext :(NSManagedObjectContext*) managedObjectContext {
+    if (managedObjectContext != nil) {
+        NSError *error = nil;
+        if (managedObjectContext.hasChanges &&
+            ![managedObjectContext save:&error]) {
+            // Replace this implementation with code to handle the error
+            // appropriately.
+            // abort() causes the application to generate a crash log and terminate.
+            // You should not use this function in a shipping application, although it
+            // may be useful during development.
+            NSLog(@"Unresolved error %@, %@", error, error.userInfo);
+            abort();
+        }
+    }
+}
 @end
