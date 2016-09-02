78f9a0dd26347996030c329406d1ffac5d1408b2
lapfelix <felix@lapal.me>
2016-09-02 17:38:50 -0400

    Implement basic Contact screen

--- okto_ios/InfoViewController.m
+++ okto_ios/InfoViewController.m
@@ -7,8 +7,19 @@
 //
 
 #import "InfoViewController.h"
+#import "AppDelegate.h"
+#import "ContactInfo+Methods.h"
+#import <IntentKit/INKMailHandler.h>
+#import <IntentKit/INKTwitterHandler.h>
+#import <IntentKit/INKFacebookHandler.h>
+#import <IntentKit/INKMapsHandler.h>
 
-@interface InfoViewController ()
+@import CoreData;
+
+@interface InfoViewController ()<NSFetchedResultsControllerDelegate>
+
+@property (nonatomic, strong) NSFetchedResultsController *fetchedResultsController;
+@property (copy, nonatomic) ContactInfo *contactInfo;
 
 @end
 
@@ -17,6 +28,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [self initializeFetchedResultsController];
+    [self fetchUpdatedContactInfo];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +38,111 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)mailButtonTap:(id)sender {
+    if (self.contactInfo.email.length != 0) {
+        INKMailHandler *mailHandler = [INKMailHandler new];
+        [[mailHandler sendMailTo:self.contactInfo.email] presentModally];
+    } else {
+        //TODO: handle this
+    }
 }
-*/
+
+- (IBAction)phoneButtonTap:(id)sender {
+    if (self.contactInfo.phone.length != 0) {
+        NSURL *phoneUrl = [NSURL URLWithString:[NSString  stringWithFormat:@"telprompt:%@",self.contactInfo.phone]];
+        if ([[UIApplication sharedApplication] canOpenURL:phoneUrl]) {
+            [[UIApplication sharedApplication] openURL:phoneUrl];
+        } else
+        {
+            //TODO: handle this (iPad or iPod)
+        }
+    } else {
+        //TODO: handle this
+    }
+}
+
+- (IBAction)snapchatButtonTap:(id)sender {
+    if (self.contactInfo.snapchatUserName.length != 0) {
+        NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"snapchat://add/%@",self.contactInfo.snapchatUserName]];
+        if ([[UIApplication sharedApplication] canOpenURL:url]) {
+            [[UIApplication sharedApplication] openURL:url];
+        } else {
+            //TODO: handle this
+            
+        }
+    } else {
+        //TODO: handle this
+    }
+}
+
+- (IBAction)twitterButtonTap:(id)sender {
+    if (self.contactInfo.twitterUserName.length != 0) {
+        INKTwitterHandler *twitterHandler = [INKTwitterHandler new];
+        [[twitterHandler showUserWithScreenName:self.contactInfo.twitterUserName] presentModally];
+    } else {
+        //TODO: handle this
+    }
+}
+
+- (IBAction)facebookButtonTap:(id)sender {
+    if (self.contactInfo.facebookUserName.length != 0) {
+        INKFacebookHandler *facebookHandler = [INKFacebookHandler new];
+        [[facebookHandler showProfileWithId:self.contactInfo.facebookUserName] presentModally];
+    } else {
+        //TODO: handle this
+    }
+}
+
+- (IBAction)mapsButtonTap:(id)sender {
+    if (self.contactInfo.physicalAddress.length != 0) {
+        INKMapsHandler *mapsHandler = [INKMapsHandler new];
+        [[mapsHandler searchForLocation:self.contactInfo.physicalAddress] presentModally];
+    } else {
+        //TODO: handle this
+    }
+}
+
+- (IBAction)websiteButtonTap:(id)sender {
+    
+}
+
+- (void)fetchUpdatedContactInfo {
+    NSArray *fetchedObjects = [[self fetchedResultsController] fetchedObjects];
+    
+    if (fetchedObjects.count > 0) {
+        _contactInfo = fetchedObjects[0];
+    } else {
+        self.contactInfo = nil;
+    }
+}
+
+#pragma mark - Fetched Results Controller Delegate methods
+
+- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
+    [self fetchUpdatedContactInfo];
+}
+
+#pragma mark - Core Data stack
+
+- (void)initializeFetchedResultsController {
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:[ContactInfo entityName]];
+    
+    NSSortDescriptor *idSort = [NSSortDescriptor sortDescriptorWithKey:@"email" ascending:YES];
+    
+    [request setSortDescriptors:@[idSort]];
+    
+    NSManagedObjectContext *moc = ((AppDelegate *)UIApplication.sharedApplication.delegate).managedObjectContext;
+    
+    [self setFetchedResultsController:[[NSFetchedResultsController alloc] initWithFetchRequest:request managedObjectContext:moc sectionNameKeyPath:nil cacheName:nil]];
+    [[self fetchedResultsController] setDelegate:self];
+    
+    NSError *error = nil;
+    if (![[self fetchedResultsController] performFetch:&error]) {
+        NSLog(@"Failed to initialize FetchedResultsController: %@\n%@", [error localizedDescription], [error userInfo]);
+        abort();
+    }
+}
+
+
 
 @end
