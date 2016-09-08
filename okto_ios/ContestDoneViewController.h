124ef16b4c7da7315cc900c24c13154db0257224
Guillaume Paquet <paquetgp@gmail.com>
2016-09-07 22:08:04 -0400

    Add contest view controllers

---
+++ okto_ios/ContestDoneViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContestDoneViewController.h
+//  okto_ios
+//
+//  Created by Guillaume Paquet on 2016-09-07.
+//  Copyright © 2016 Felix Lapalme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContestDoneViewController : UIViewController
+
+@end
