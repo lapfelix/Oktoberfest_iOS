e4deab3c646a9a4258a1dd925c1493a398c4efe3
Felix Lapalme <lap.felix@gmail.com>
2016-09-14 19:00:06 -0400

    Add and design LoadingViewController

---
+++ okto_ios/LoadingViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoadingViewController.h
+//  okto_ios
+//
+//  Created by Felix Lapalme on 2016-09-14.
+//  Copyright © 2016 Felix Lapalme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoadingViewController : UIViewController
+
+@end
---
+++ okto_ios/LoadingViewController.m
@@ -0,0 +1,40 @@
+//
+//  LoadingViewController.m
+//  okto_ios
+//
+//  Created by Felix Lapalme on 2016-09-14.
+//  Copyright © 2016 Felix Lapalme. All rights reserved.
+//
+
+#import "LoadingViewController.h"
+#import "OKTAPIWrapper.h"
+#import "RootViewController.h"
+@import Fabric;
+
+@interface LoadingViewController ()
+
+@end
+
+@implementation LoadingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
