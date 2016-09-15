6ecb67a9e5ecd4371b65fd97d1154d4b3a2cb74f
Felix Lapalme <lap.felix@gmail.com>
2016-09-14 20:15:11 -0400

    Add basic main contest logic

--- okto_ios/ContestStepViewController.m
+++ okto_ios/ContestStepViewController.m
@@ -10,6 +10,7 @@
 #import "AppDelegate.h"
 #import "ContestStep+Methods.h"
 #import "OKTAppearance.h"
+#import <SDWebImage/UIImageView+WebCache.h>
 
 #import <RKDropdownAlert/RKDropdownAlert.h>
 
@@ -48,6 +49,13 @@ long totalSteps = 8;
                                              selector:@selector(keyboardWillBeHidden:)
                                                  name:UIKeyboardWillHideNotification
                                                object:nil];
+    
+    [self initialSetup];
+}
+
+- (void)initialSetup {
+    [self loadCurrentQuestionImage];
+    [self updateProgressBar];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -55,25 +63,44 @@ long totalSteps = 8;
     // Dispose of any resources that can be recreated.
 }
 
+- (void)updateProgressBar {
+    [self.stepProgressView setProgress:(float)currentStep/totalSteps animated:YES];
+}
+
+- (void)loadCurrentQuestionImage {
+    ContestStep *currentContestStep = contestSteps[currentStep];
+    [self.beerImage sd_setImageWithURL:[NSURL URLWithString:currentContestStep.questionImageUrl]];
+}
+
+- (void)loadCurrentAnswerImage {
+    ContestStep *currentContestStep = contestSteps[currentStep];
+    [self.beerImage sd_setImageWithURL:[NSURL URLWithString:currentContestStep.answerImageUrl]];
+}
+
 - (IBAction)validateTap:(UIButton *)sender {
     ContestStep *currentContestStep = contestSteps[currentStep];
     
-    if ([[_answerTextField.text lowercaseString] isEqualToString:[currentContestStep.answer lowercaseString]]) {
+    if ([[self.answerTextField.text lowercaseString] isEqualToString:[currentContestStep.answer lowercaseString]]) {
         
+        [self loadCurrentAnswerImage];
         currentStep++;
         
         if (currentStep >= totalSteps) {
-            
-            [RKDropdownAlert title:@"Mauvaise réponse..." message:@"Essayez à nouveau!" backgroundColor:[OKTAppearance greenColor] textColor:UIColor.whiteColor time:1 completionHandler:^{
+            [RKDropdownAlert title:@"Félicitations!" message:@"Vous avez réussi!" backgroundColor:[OKTAppearance greenColor] textColor:UIColor.whiteColor time:2.5 completionHandler:^{
                 [self showSuccessStep];
+            }];
+        } else {
+            [RKDropdownAlert title:@"Bravo!" message:@"Bonne réponse!" backgroundColor:[OKTAppearance greenColor] textColor:UIColor.whiteColor time:2.5 completionHandler:^{
+                [self loadCurrentQuestionImage];
             }];
         }
 
-        [_stepProgressView setProgress:(float)currentStep/totalSteps];
+        [self updateProgressBar];
         [self resetLabels];
         
     } else {
         [RKDropdownAlert title:@"Mauvaise réponse..." message:@"Essayez à nouveau!" backgroundColor:[OKTAppearance yellowColor] textColor:UIColor.blackColor time:2];
+        [self resetLabels];
     }
 }
 
@@ -110,16 +137,14 @@ long totalSteps = 8;
     }
 }
 
-- (void)keyboardDidShow:(NSNotification *)notification
-{
+- (void)keyboardDidShow:(NSNotification *)notification {
     NSDictionary* info = [notification userInfo];
     CGRect kbRect = [[info objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
     
     self.bottomLayoutConstraint.constant = CGRectGetHeight(kbRect);
 }
 
-- (void)keyboardWillBeHidden:(NSNotification *)notification
-{
+- (void)keyboardWillBeHidden:(NSNotification *)notification {
     NSDictionary* info = [notification userInfo];
     CGRect kbRect = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue];
     
