8ed6609fd25ee7838acb8c10a09b8351b2e19fb8
Felix Lapalme <lap.felix@gmail.com>
2016-09-17 23:53:54 -0400

    Periodically refresh server data every 2 minutes

--- okto_ios/AppDelegate.m
+++ okto_ios/AppDelegate.m
@@ -92,7 +92,9 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     
-    [[OKTAPIWrapper sharedWrapper] syncWithServer];
+    [NSTimer scheduledTimerWithTimeInterval:120 repeats:YES block:^(NSTimer * _Nonnull timer) {
+        [[OKTAPIWrapper sharedWrapper] syncWithServer];
+    }];
 }
 
 
